559dbebf38e8671ebd3329fe73fe49fd5de51ebb
luoleicn <luoleicn@b6497320-2115-11df-b9f8-bfe9128881a6>
2010-03-15 01:28:13 +0000

    增加btree－fileoperation，可以生成一个btree文件，读一个btree文件，读写一个btree文件

---
+++ trunk/c-cpp/bTree_fileOperation/itemtype.h
@@ -0,0 +1,33 @@
+/* Filename:  itemtype.h
+
+   Programmer:  Br. David Carlson
+
+   Date:  May 23, 1999 and June 27, 2000                                                    
+
+   This header file sets up ItemType and associated items.
+*/
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+const int KeyFieldMax = 12;
+
+const int KFMaxPlus1 = KeyFieldMax + 1;
+
+const int DataFieldMax = 36;
+
+const int DFMaxPlus1 = DataFieldMax + 1;
+
+
+typedef char KeyFieldType[KFMaxPlus1];
+
+typedef char DataFieldType[DFMaxPlus1];
+
+typedef struct
+   {
+   KeyFieldType KeyField;
+   DataFieldType DataField;
+   } ItemType;
+
