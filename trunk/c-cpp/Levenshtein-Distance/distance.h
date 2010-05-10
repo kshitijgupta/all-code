afd2ef6d2d1338df3fee99428609a68f57403336
luoleicn <luoleicn@b6497320-2115-11df-b9f8-bfe9128881a6>
2010-05-10 00:56:02 +0000

    Levenshtein-Distance

---
+++ trunk/c-cpp/Levenshtein-Distance/distance.h
@@ -0,0 +1,14 @@
+/*
+ * 最大编辑距离算法
+ * http://www.merriampark.com/ld.htm
+ */
+class Distance
+{
+    public:
+        int LD (char const *s, char const *t);
+    private:
+        int Minimum (int a, int b, int c);
+        int *GetCellPointer (int *pOrigin, int col, int row, int nCols);
+        int GetAt (int *pOrigin, int col, int row, int nCols);
+        void PutAt (int *pOrigin, int col, int row, int nCols, int x);
+}; 
