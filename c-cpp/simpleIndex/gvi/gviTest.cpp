33746afaf78af4106105808be28b4bd65220bfa2
luoleicn <luoleicn@b6497320-2115-11df-b9f8-bfe9128881a6>
2010-08-05 10:52:22 +0000

    add some comments on simpleIndex/gvi

--- c-cpp/simpleIndex/gvi/gviTest.cpp
+++ c-cpp/simpleIndex/gvi/gviTest.cpp
@@ -20,7 +20,9 @@ int main(int args, char** argv){
     unsigned int int4[] = {1, 15, 511, 13107};
     unsigned char buf[100] = {0};
 
-    writeGVInt(buf, int4);
+    int size = writeGVInt(buf, int4);
+
+    cout << "写入到buf数组中" << size << "个字节" << endl;
 
     memset(int4, 0, sizeof(int)*4);
 
